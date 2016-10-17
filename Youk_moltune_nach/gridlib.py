38387b4cd4f7806d7cfdfd4030dce1966343f2f4
Nexia23 <karnetzki.maxim@gmx.de>
2016-10-17 12:26:16 +0200

    bugfix_ncells

--- Youk_moltune_nach/gridlib.py
+++ Youk_moltune_nach/gridlib.py
@@ -4,13 +4,20 @@ import numpy as np
 import math
 import classes as cl
 
-def border(pt):
 
-    fenc = pos[pt][0] + x *pos[pt][1]
+def initialize():                       #creats grid on which cells placed
+    ce = 0
+    for row in range(len(x_c)):
+        for col in range(len(y_c)):
+            pos[ce] = [x_c[row], y_c[col]]
+            ce += 1
 
-    if fenc - radius < 0:
+
+def border(pt):                         #checks for outofbounds around edges#
+
+    if pt - x * radius < 0:
         return False
-    if fenc + radius > (x) ** 2:
+    if pt + x * radius > ncells:
         return False
 
     if pos[pt][0]-radius < 0 :
@@ -22,36 +29,12 @@ def border(pt):
         return True
 
 
-
-
-def occupy(m):                              #cellplacement
-
-    if isinstance(c_ary[m], cl.cell):         #check if cell there safety
-        rad = c_ary[m].radius
-
-        for h in range(-rad,rad+1):         # go along the (row)
-            for b in range(-rad,rad+1):     #outer quadrate of the circle r=rad (col)
-
-                poi = m + h*x + b  # position in the chain
-
-                eq = math.sqrt((pos[poi][0] - pos[m][0]) ** 2 + (pos[poi][1] - pos[m][1]) ** 2)
-
-                if eq <= rad:               #if smaller or same than position inside the cell
-
-                    if resident[poi] == 0 :
-                        resident[poi] = c_ary[m].mid
-
-                elif poi==m:
-                    pass
-
-
-
-
 def checkneighbor(p):                   # check if can be placed
     print('check')
     ind = True
 
     if p >= (ncells)**2 or p < 0:
+        print ('out')
         return False
 
     if resident[p]!=0:
@@ -65,7 +48,9 @@ def checkneighbor(p):                   # check if can be placed
         for b in range(-rad, rad + 1):                  # outer quadrate of the circle r=rad (col)
 
             poi = p + h * x + b                         # position in the chain
-            if poi >= (ncells) ** 2 or poi < 0:
+            if poi >= ncells:
+                return False
+            if poi < 0:
                 return False
 
             eq = math.sqrt((pos[poi][0] - pos[p][0]) ** 2 + ( pos[poi][1] - pos[p][1]) ** 2)
@@ -78,13 +63,39 @@ def checkneighbor(p):                   # check if can be placed
     return ind
 
 
+
+def occupy(m):                              #cellplacement
+
+    if isinstance(c_ary[m], cl.cell):         #check if cell there safety
+        rad = c_ary[m].radius
+
+        for h in range(-rad,rad+1):         # go along the (row)
+            for b in range(-rad,rad+1):     #outer quadrate of the circle r=rad (col)
+
+                poi = m + h*x + b  # position in the chain
+                if poi == m:
+                    pass
+
+                eq = math.sqrt((pos[poi][0] - pos[m][0]) ** 2 + (pos[poi][1] - pos[m][1]) ** 2)
+
+                if eq <= rad:               #if smaller or same than position inside the cell
+
+                    if resident[poi] == 0 :
+                        resident[poi] = c_ary[m].mid
+
+
+
+
+
+
+
 def move(p):
     pass
     #print('shiiit'+ str(p))
 
 #Parametersettings#
 
-x=5                               #sets gridsize
+x=10                              #sets gridsize
 n=1.0                             #set chance of cells n probability
 place=0.5                         #set on_state cells n probability
 C_on=13.0                         #signalconcentration of activ cel#
@@ -93,7 +104,7 @@ feedback = 0                      #positiv(1) or negative(0) feedback#
 min_cell=5                        #set minimum of cellneigbors for new cellcreation#
 radius=2
 
-cc=0                              #for arrays entrykey
+cc=0                              #the entrykey for dics
 
 
 # Dimensions
@@ -115,15 +126,8 @@ C_i = np.zeros(ncells)       #concentration of each cell at time i #
 C_print=np.zeros(ncells)     #actual concentrations at position cells + neighbor#
 
 resident=np.zeros(ncells)    #spot has cell or not| int references#
-c_ary={}       #cell-class ref#
-pos={}
-
-def initialize():                       #creats grid on which cells placed
-    ce = 0
-    for row in range(len(x_c)):
-        for col in range(len(y_c)):
-            pos[ce] = [x_c[row], y_c[col]]
-            ce += 1
+c_ary={}                     #cell-class ref#
+pos={}                       #dic for grid#
 
 
 initialize()
