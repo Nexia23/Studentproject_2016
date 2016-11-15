550cefd2edcc184721c587b76cc61c8e3f176245
Nexia23 <karnetzki.maxim@gmx.de>
2016-11-15 14:03:38 +0100

    forces_pics

--- Youk_moltune_nach/gridlib.py
+++ Youk_moltune_nach/gridlib.py
@@ -4,23 +4,20 @@ import numpy as np
 import math
 import classes as cl
 import vtktools
-#Parametersettings#
 
+#Parametersettings#
 x=100                              #sets gridsize
 n=1.0                             #set chance of cells n probability
 place=0.5                         #set on_state cells n probability
 C_on=13.0                         #signalconcentration of activ cel#
 K =  18.0                         #threshold c#
 feedback = 0                      #positiv(1) or negative(0) feedback#
-min_cell=5                        #set minimum of cellneigbors for new cellcreation#
 
 radius=1                          #set intial radius of cell
-
 maxrad=radius*2                   #max r which cell can have before division#
-
-
+k=1.0                             #factor how close neighboring cells can be
 g_rate=1                          #growthrate of radius
-k=0.9                             #factor how close neighboring cells can be
+
 # Dimensions
 nx, ny, nz = x, x, 1
 lx, ly, lz = 100.0, 100.0, 0.1
@@ -45,6 +42,9 @@ pos={}                        #dic for grid [0]=x_axis [1]=y_axis#
 
 print(npoints)
 
+fx = np.zeros(ncells)
+fy = np.zeros(ncells)
+fz = np.zeros(ncells)
 
 def initialize():                               #creats grid on which cells are placed by chance
 
@@ -53,7 +53,7 @@ def initialize():                               #creats grid on which cells are 
         for col in range(len(x_c)):
             pos[ce] = [x_c[col], y_c[row]]
             ce += 1
-
+    c_num=0
     for cc in range(npoints):                    #cells being placed#
         put = rd.random()
 
@@ -67,9 +67,9 @@ def initialize():                               #creats grid on which cells are 
             if checkneighbor(cc):               #check if cell can be placed#
 
                 if put <= n:
-
-                    c_ary[cc] = cl.cell(cc, 'ecoli ', radius, stategamble(), pos[cc][0], pos[cc][1], 1)
-                    occupy(cc)
+                    c_num+=1
+                    c_ary[c_num] = cl.cell(c_num, 'ecoli ', radius, stategamble(), pos[cc][0], pos[cc][1], 2)
+                    occupy(cc,c_num)
 
 
 def stategamble():                      #prodces random booleanvalue according to place
@@ -77,24 +77,23 @@ def stategamble():                      #prodces random booleanvalue according t
 
     if hp <= place:
         return True
-
     else:
         return False
 
 def border(pt):                         #checks for outofbounds around edges #
 
 
-    if pos[pt][0]-maxrad < 0 :
+    if pos[pt][0] - radius < 0 :
         print ('hey b_3')
         return False
-    if pos[pt][1]-maxrad < 0 :
+    if pos[pt][1] - radius < 0 :
         print ('hey b_o4')
         return False
 
-    if pos[pt][0] + maxrad > lx:
+    if pos[pt][0] + radius > lx:
         print ('hey b_5')
         return False
-    if pos[pt][1] + maxrad > ly:
+    if pos[pt][1] + radius > ly:
         print ('hey b_6')
         return False
 
@@ -123,8 +122,6 @@ def checkneighbor(p):                   #check if cell can be placed#
 def outersq(p):
 
     rad = radius
-    if p in c_ary:                      #check if cell there possible different r#
-        rad = c_ary[p].radius
 
     for h in range(-rad, rad + 1):      #go along the (row)
         for b in range(-rad, rad + 1):  #outer quadrate of the circle r=rad (col)
@@ -147,10 +144,10 @@ def outersq(p):
 
     return True
 
-def occupy(m):                              #cellplacement
+def occupy(m,id):                              #cellplacement
 
-    if isinstance(c_ary[m], cl.cell):       #get cell_r for placement#
-        rad = c_ary[m].radius
+    if isinstance(c_ary[id], cl.cell):       #get cell_r for placement#
+        rad = radius
 
         if maxrad > dx or maxrad > dy:
 
@@ -161,24 +158,30 @@ def occupy(m):                              #cellplacement
                     if poi == m:
                         pass
 
-                    eq = math.sqrt((pos[poi][0] - c_ary[m].xcor) ** 2 + (pos[poi][1] - c_ary[m].ycor) ** 2)
+                    eq = math.sqrt((pos[poi][0] - c_ary[id].xcor) ** 2 + (pos[poi][1] - c_ary[id].ycor) ** 2)
 
                     if eq <= rad:               #if smaller or same than position inside the cell
 
                         if resident[poi] == 0 :
-                            resident[poi] = c_ary[m].mid
+                            resident[poi] = c_ary[id].mid
         else:
-            resident[m] = c_ary[m].mid
+            resident[m] = c_ary[id].mid
 
 
 def force():                                     #calculates movement by forcecalc of cells pushing
 
-    d_x = np.zeros(ncells)
-    d_y = np.zeros(ncells)
-    d_z = np.zeros(ncells)
-
     for elem in c_ary:
+        fx[elem]=0
+        fy[elem]=0
+        fz[elem]=0
         for oths in c_ary:
+
+            if c_ary[elem].xcor <= c_ary[elem].radius:
+                c_ary[elem].xcor = c_ary[elem].radius
+            if c_ary[elem].ycor <= c_ary[elem].radius:
+                c_ary[elem].ycor = c_ary[elem].radius
+            if c_ary[elem].zcor <= c_ary[elem].radius:
+                c_ary[elem].zcor = c_ary[elem].radius
 
             sum=np.square(c_ary[oths].xcor-c_ary[elem].xcor)\
             +np.square(c_ary[oths].ycor-c_ary[elem].ycor)\
@@ -191,32 +194,37 @@ def force():                                     #calculates movement by forceca
             if d_n==0:
                 pass
 
-            elif d_n < R :                           #checks if cell[elem] is pushed by cell[y] only when d_n < R
+            elif d_n < k*R :                           #checks if cell[elem] is pushed by cell[y] only when d_n < R
                 fac = (k * R) - d_n
 
-                xdd = (c_ary[oths].xcor - c_ary[elem].xcor)/d_n
-                ydd = (c_ary[oths].ycor - c_ary[elem].ycor) / d_n
-                zdd = (c_ary[oths].zcor - c_ary[elem].zcor) / d_n
+                xdd = -(c_ary[oths].xcor - c_ary[elem].xcor)/d_n
+                ydd = -(c_ary[oths].ycor - c_ary[elem].ycor) / d_n
+                zdd = -(c_ary[oths].zcor - c_ary[elem].zcor) / d_n
 
-                d_x[elem] = xdd * fac + d_x[elem]
-                d_y[elem] = ydd * fac + d_y[elem]
-                d_z[elem] = zdd * fac + d_z[elem]
+                fx[elem] = xdd * fac + fx[elem]
+                fy[elem] = ydd * fac + fy[elem]
+                fz[elem] = zdd * fac + fz[elem]
 
 
-
-    return d_x , d_y , d_z
 
 def move():
 
-    delx,dely,delz = force()
-
+    force()
+    dt = 0.1 / max(max(fx),max(fy),max(fz))
     for elem in c_ary:
 
-        c_ary[elem].xcor = c_ary[elem].xcor + delx[elem]
-        c_ary[elem].ycor = c_ary[elem].ycor + dely[elem]
-        c_ary[elem].zcor = c_ary[elem].zcor + delz[elem]
+        c_ary[elem].xcor = c_ary[elem].xcor + dt*fx[elem]
+        c_ary[elem].ycor = c_ary[elem].ycor + dt*fy[elem]
+        c_ary[elem].zcor = c_ary[elem].zcor + dt*fz[elem]
 
-    return delx, dely, delz
+        if c_ary[elem].xcor <= c_ary[elem].radius:
+            c_ary[elem].xcor = c_ary[elem].radius
+        if c_ary[elem].ycor <= c_ary[elem].radius:
+            c_ary[elem].ycor = c_ary[elem].radius
+        if c_ary[elem].zcor <= c_ary[elem].radius:
+            c_ary[elem].zcor = c_ary[elem].radius
+
+
 
 def divide(p):
     pass
@@ -224,7 +232,6 @@ def divide(p):
 def growth(p):
 
     c_ary[p].radius = c_ary[p].radius + g_rate
-
 
 def event():                                #what happens to cell in time step#
 
@@ -236,8 +243,9 @@ def event():                                #what happens to cell in time step#
         elif c_ary[elem].status:
             growth(elem)                    #if cell=on -> grows#
 
-    f1,f2,f3=move()
-    return f1,f2,f3
+    move()
+
+
 
 
 
@@ -253,16 +261,41 @@ def update(end):
     initialize()
 
     for step in time:
+       event()
+       pic(step)
 
-        f1,f2,f3=event()
-    return f1,f2,f3
+def pic(a):
+    x_list = []
+    y_list = []
+    z_list = []
+    r_list = []
+    F_x = []
+    F_y = []
+    F_z = []
+
+
+    vtk_writer = vtktools.VTK_XML_Serial_Unstructured()
+
+    for elem in c_ary:
+        x_list.append(c_ary[elem].xcor)
+        y_list.append(c_ary[elem].ycor)
+        z_list.append(c_ary[elem].zcor)
+        r_list.append(c_ary[elem].radius)
+        F_x.append(fx[elem])
+        F_y.append(fy[elem])
+        F_z.append(fz[elem])
+
+    vtk_writer.snapshot("cell_arrangements"+str(a)+".vtu", x_list, y_list, z_list, radii=r_list, x_force=F_x, y_force=F_y,
+                        z_force=F_z)
+    vtk_writer.writePVD("cell_arrangements"+str(a)+".pvd")
 
 
 
+#run program
 
+update(1)
 
-fx,fy,fz=update(1)
-
+"""
 rei = ''
 bla=0
 for i in range(0, npoints):
@@ -290,30 +323,4 @@ for i in range(0, npoints):
     elif pos[i][0]==lx and i!=0:
         print rei
         rei = ''
-
-x_list=[]
-y_list=[]
-z_list=[]
-r_list=[]
-F_x=[]
-F_y=[]
-F_z=[]
-
-vtk_writer = vtktools.VTK_XML_Serial_Unstructured()
-
-for elem in c_ary:
-    x_list.append(c_ary[elem].xcor)
-    y_list.append(c_ary[elem].ycor)
-    z_list.append(0.0)
-    r_list.append(c_ary[elem].radius)
-    F_x.append(fx[elem])
-    F_y.append(fy[elem])
-    F_z.append(fz[elem])
-
-print (x_list)
-print(y_list)
-print (r_list)
-
-
-vtk_writer.snapshot("cell_arrangements.vtu",  x_list, y_list, z_list, radii = r_list, x_force = F_x, y_force = F_y, z_force = F_z )
-vtk_writer.writePVD("cell_arrangements.pvd")
+ """
